1a67008ec42a0ee36383dc38a1ad1ba6bfe73993
Mario Murillo <mariotinoco85@gmail.com>
2025-04-21 17:58:42 -0600

    [ADD]->Se agrego el formulario de producto con su url y su vista

--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views import generic
+from .forms import ProducForm
 
 # Create your views here.
+class ProductFormView(generic.FormView):
+    template_name = "products/add_product.html"
+    form_class = ProducForm
+    
